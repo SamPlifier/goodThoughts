25de77ccba0d14c0d926dc4e540bd2fb4128b0d7
SamDC <sam.davis.castro@gmail.com>
2017-06-23 16:00:04 -0400

    Fixed improperly closed tag: was causing page to be wider than expected.

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 const navLink = $('.topNav')[0];
 const nav = $('#myTopNav')[0];
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if (target.length) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top - 50
+        }, 1000);
+    }
+});
 
 function addRespClass() {
     if (nav.className === 'topNav') {
